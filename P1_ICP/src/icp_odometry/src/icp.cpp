76c468cf5a6b847fca0be79163ee4f1744f4499c
GLGDLY <tzlgdly@gmail.com>
2023-10-10 13:23:54 +0800

    finally working shit

--- P1_ICP/src/icp_odometry/src/icp.cpp
+++ P1_ICP/src/icp_odometry/src/icp.cpp
@@ -23,97 +23,96 @@ Eigen::Matrix4d icp_registration(pcl::PointCloud<pcl::PointXYZ>::Ptr src_cloud,
 	pcl::transformPointCloud(*src_cloud, *transformed_cloud, transformation);
 
 	uint32_t iter = 0;
-  std::cout<<"start it\n";
+  	std::cout<<"start it\n";
 	while (iter < params::max_iterations) {
-    std::cout<<"it"<<iter <<": ";
-			pcl::PointCloud<pcl::PointXYZ>::Ptr correspondences(new pcl::PointCloud<pcl::PointXYZ>);
-			std::vector<int> correspondences_indices;
-			std::vector<float> correspondences_distances;
+		std::cout<<"it"<<iter <<": ";
+		pcl::PointCloud<pcl::PointXYZ>::Ptr correspondences(new pcl::PointCloud<pcl::PointXYZ>);
+		std::vector<int> correspondences_indices;
+		std::vector<float> correspondences_distances;
 
-			for (int i = 0; i < transformed_cloud->points.size(); i++) {
-				pcl::PointXYZ searchPoint = transformed_cloud->points[i];
-				std::vector<int> pointIdxNKNSearch(1);
-				std::vector<float> pointNKNSquaredDistance(1);
-				kdtree.nearestKSearch(searchPoint, 1, pointIdxNKNSearch, pointNKNSquaredDistance);
-				correspondences->points.push_back(tar_cloud->points[pointIdxNKNSearch[0]]);
-				correspondences_indices.push_back(pointIdxNKNSearch[0]);
-				correspondences_distances.push_back(pointNKNSquaredDistance[0]);
+		for (int i = 0; i < transformed_cloud->points.size(); i++) {
+			pcl::PointXYZ searchPoint = transformed_cloud->points[i];
+			std::vector<int> pointIdxNKNSearch(1);
+			std::vector<float> pointNKNSquaredDistance(1);
+			kdtree.nearestKSearch(searchPoint, 1, pointIdxNKNSearch, pointNKNSquaredDistance);
+			correspondences->points.push_back(tar_cloud->points[pointIdxNKNSearch[0]]);
+			correspondences_indices.push_back(pointIdxNKNSearch[0]);
+			correspondences_distances.push_back(pointNKNSquaredDistance[0]);
+		}
+
+		pcl::PointCloud<pcl::PointXYZ>::Ptr correspondences_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+		std::vector<int> correspondences_filtered_indices;
+		std::vector<float> correspondences_filtered_distances;
+
+		for (int i = 0; i < correspondences->points.size(); i++) {
+			if (correspondences_distances[i] < params::max_distance) {
+				correspondences_filtered->points.push_back(correspondences->points[i]);
+				correspondences_filtered_indices.push_back(correspondences_indices[i]);
+				correspondences_filtered_distances.push_back(correspondences_distances[i]);
 			}
-
-			pcl::PointCloud<pcl::PointXYZ>::Ptr correspondences_filtered(new pcl::PointCloud<pcl::PointXYZ>);
-			std::vector<int> correspondences_filtered_indices;
-			std::vector<float> correspondences_filtered_distances;
-
-			for (int i = 0; i < correspondences->points.size(); i++) {
-				if (correspondences_distances[i] < params::max_distance) {
-					correspondences_filtered->points.push_back(correspondences->points[i]);
-					correspondences_filtered_indices.push_back(correspondences_indices[i]);
-					correspondences_filtered_distances.push_back(correspondences_distances[i]);
-				}
-			}
+		}
+	
+		Eigen::Matrix<double, 3, Eigen::Dynamic> src(3, correspondences_filtered->points.size());
+		Eigen::Matrix<double, 3, Eigen::Dynamic> tar(3, correspondences_filtered->points.size());
+	
+		for (int i = 0; i < correspondences_filtered->points.size(); i++) {
+			src(0, i) = transformed_cloud->points[i].x;
+			src(1, i) = transformed_cloud->points[i].y;
+			src(2, i) = transformed_cloud->points[i].z;
+			tar(0, i) = correspondences_filtered->points[i].x;
+			tar(1, i) = correspondences_filtered->points[i].y;
+			tar(2, i) = correspondences_filtered->points[i].z;
+		}
       
-			Eigen::Matrix<double, 3, Eigen::Dynamic> src(3, correspondences_filtered->points.size());
-			Eigen::Matrix<double, 3, Eigen::Dynamic> tar(3, correspondences_filtered->points.size());
-      
-			for (int i = 0; i < correspondences_filtered->points.size(); i++) {
-				src(0, i) = transformed_cloud->points[i].x;
-				src(1, i) = transformed_cloud->points[i].y;
-				src(2, i) = transformed_cloud->points[i].z;
-				tar(0, i) = correspondences_filtered->points[i].x;
-				tar(1, i) = correspondences_filtered->points[i].y;
-				tar(2, i) = correspondences_filtered->points[i].z;
+        std::cout<<"dicentrolize   ";
+		Eigen::Matrix<double, 3, 1> src_mean = src.rowwise().mean();
+		Eigen::Matrix<double, 3, 1> tar_mean = tar.rowwise().mean();
+
+
+		for (int i = 0; i < correspondences_filtered->points.size(); i++) {
+			src.col(i) -= src_mean;
+			tar.col(i) -= tar_mean;
+		}
+
+        std::cout<<"SVD    ";
+		Eigen::Matrix3d H = src * tar.transpose();
+
+		Eigen::JacobiSVD<Eigen::Matrix3d> svd(H, Eigen::ComputeFullU | Eigen::ComputeFullV);
+		Eigen::Matrix3d U = svd.matrixU();
+		Eigen::Matrix3d V = svd.matrixV();
+
+		Eigen::Matrix3d Rotation = V * U.transpose();
+
+		if (Rotation.determinant() < 0) {
+			for (int i = 0; i < 3; i++) {
+				U(i, 2) *= -1;
 			}
-      
-      std::cout<<"dicentrolize   ";
-			Eigen::Matrix<double, 3, 1> src_mean = src.rowwise().mean();
-			Eigen::Matrix<double, 3, 1> tar_mean = tar.rowwise().mean();
+			Rotation = V * U.transpose();
+		}
+
+		Eigen::Vector3d Translation = tar_mean - Rotation * src_mean;
+
+		Eigen::Matrix4d update_transformation = Eigen::Matrix4d::Identity();
+		update_transformation.block<3, 3>(0, 0) = Rotation;
+		update_transformation.block<3, 1>(0, 3) = Translation;
 
 
-			for (int i = 0; i < correspondences_filtered->points.size(); i++) {
-				src.col(i) -= src_mean;
-				tar.col(i) -= tar_mean;
-			}
+		std::cout<<"Check cov  ";
+				Eigen::Matrix4d _transformation = update_transformation * transformation;
+		
+		if ((transformation - prev_transformation).norm() < params::max_distance) {
+			break;
+		} else {
+			prev_transformation = transformation;
+		}
 
-      std::cout<<"SVD    ";
-			Eigen::Matrix3d H = src * tar.transpose();
+		pcl::transformPointCloud(*src_cloud, *transformed_cloud, transformation);
+		std::cout<<"it"<<iter<<" end\n";
+		iter++;
 
-			Eigen::JacobiSVD<Eigen::Matrix3d> svd(H, Eigen::ComputeFullU | Eigen::ComputeFullV);
-			Eigen::Matrix3d U = svd.matrixU();
-			Eigen::Matrix3d V = svd.matrixV();
-
-			Eigen::Matrix3d Rotation = V * U.transpose();
-
-			if (Rotation.determinant() < 0) {
-				for (int i = 0; i < 3; i++) {
-					U(i, 2) *= -1;
-				}
-				Rotation = V * U.transpose();
-			}
-
-			Eigen::Vector3d Translation = tar_mean - Rotation * src_mean;
-
-			Eigen::Matrix4d update_transformation = Eigen::Matrix4d::Identity();
-			update_transformation.block<3, 3>(0, 0) = Rotation;
-			update_transformation.block<3, 1>(0, 3) = Translation;
-
-
-      std::cout<<"Check cov  ";
-			Eigen::Matrix4d _transformation = update_transformation * transformation;
-      if (_transformation.rows() != 4 || _transformation.cols() != 4) {
-        break;
-      } else {
-        transformation = _transformation;
-      }
-      
-			if ((transformation - prev_transformation).norm() < params::max_distance) {
-				break;
-			} else {
-				prev_transformation = transformation;
-			}
-
-			pcl::transformPointCloud(*src_cloud, *transformed_cloud, transformation);
-      std::cout<<"it"<<iter<<" end\n";
-			iter++;
+		// prevent destructor error
+		correspondences.reset();
+		correspondences_filtered.reset();
 	}
 
   std::cout<<"E\n\n";
